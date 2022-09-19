fb41d643857fee7db3bf38aad6c352635a915b57
Losssaw3 <113118104+Losssaw3@users.noreply.github.com>
2022-09-19 21:15:24 +0300

    1.3

---
+++ task 1 new(solution)/1.3 new/Source.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+/**
+* \brief Функция определяющая вес тела
+* \param Первый аргумент - масса тела
+* \param Второй агрумент - ускорение свободного падения
+* \return Возвращает значение веса тела
+*/
+double GetWeight(double Mass, double GravityAcceleration);
+/**
+* \brief Функция определяющая силу тяжести
+* \param Первый аргумент - масса тела
+* \param Второй агрумент - ускорение свободного падения
+* \return Возвращает значение силы тяжести
+*/
+double GetGravityForse(double Mass, double GravityAcceleration);
+/**
+* \brief Точка входа в программу 
+* \return 0 в случае успеха
+*/
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	double GravityAcceleration = 9.8;
+	double Mass = 0.0;
+	cout << "Введите массу тела" << endl;
+	cin >> Mass;
+	if (Mass < 0)
+	{
+		cout << "Недопустимое значение массы" << endl;
+		return 1;
+	}
+	double Weight = GetWeight(Mass, GravityAcceleration);
+	double GravityForse = GetGravityForse(Mass, GravityAcceleration);
+	cout << "Сила тяжести = " << GravityForse << '\n' << "Вес тела = " << Weight << endl;
+	return 0;
+}
+double GetWeight(double Mass, double GravityAcceleration)
+{
+	return (Mass * GravityAcceleration);
+}
+double GetGravityForse(double Mass, double GravityAcceleration)
+{
+	return (Mass * GravityAcceleration);
+}
