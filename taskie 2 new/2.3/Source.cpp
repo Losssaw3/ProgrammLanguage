fed5895e8fa31d8240a3dce72213d58ccdf6ecca
Losssaw3 <113118104+Losssaw3@users.noreply.github.com>
2022-09-27 12:14:22 +0300

    2.3 переделанное

--- taskie 2 new/2.3/Source.cpp
+++ taskie 2 new/2.3/Source.cpp
@@ -11,6 +11,7 @@ const double getSides(const string& message = "");
 *\param  MessageH побуждающее сообщение
 *\return Возвращает значения  высоты , ширины отверстия
 */
+const double checkOut(const double length, const double width, const double height, const double holeWidth, const double holeHeight);
 int main()
 {
 	setlocale(LC_ALL, "Rus");
@@ -19,12 +20,13 @@ int main()
 	const double height = getSides("Введите значение высоты кирпича");
 	const double holeWidth = getSides("Введите значение ширины отверстия");
 	const double holeHeight = getSides("Введите значение высоты отверстия");
+	const int check = checkOut(length, width, height, holeWidth, holeHeight);
 	if (length < 0 or width < 0 or height < 0 or holeHeight < 0 or holeWidth < 0)
 	{
 		cout << "Были введены недопустимые значения" << endl;
 		return 1;
 	}
-	if ((width <= holeWidth) and (height <= holeHeight) or ((width <= holeWidth) and (length <= holeHeight)) or ((height <= holeWidth) and (length <= holeHeight)) or (width <= holeHeight) and (height <= holeWidth) or ((width <= holeHeight) and (length <= holeWidth)) or ((height <= holeHeight) and (length <= holeWidth)))
+	if (check == 1)
 	{
 		cout << "Кирпич пройдет!" << endl;
 	}
@@ -42,4 +44,15 @@ const double getSides(const string& message)
 	double side = 0.0;
 	cin >> side;
 	return side;
+}
+const double checkOut(const double length, const double width, const double height, const double holeWidth, const double holeHeight)
+{
+	if ((width <= holeWidth) and (height <= holeHeight) or ((width <= holeWidth) and (length <= holeHeight)) or ((height <= holeWidth) and (length <= holeHeight)) or (width <= holeHeight) and (height <= holeWidth) or ((width <= holeHeight) and (length <= holeWidth)) or ((height <= holeHeight) and (length <= holeWidth)))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
